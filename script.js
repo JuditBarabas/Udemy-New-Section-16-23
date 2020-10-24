7fa3b6aae69fd172afeb623faf33eb71e51539ba
JuditBarabas <62601033+JuditBarabas@users.noreply.github.com>
2020-10-24 23:38:42 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Change the value of num, so that "YOU GOT ME!" prints out
+const num = 102; // THIS IS THE ONLY LINE YOU SHOULD CHANGE :) 
+
+// DO NOT TOUCH ANYTHING BELOW (please) 
+if(num <= 100) {
+    if(num >= 50) {
+        console.log("HEY!");
+    }
+} else {
+    if (num < 103) {
+        if(num % 2 === 0){
+            console.log("YOU GOT ME!");
+        }
+    }
+}
+
+console.log('For code open js file');
